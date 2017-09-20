b487f8c7b89b1b088a1b3ae9e9502a9bc3d2095f
rileyp2000 <rileyp@bishopireton.org>
2017-09-20 12:52:40 -0400

    did filereader butr with errors

--- HW Assign 1/src/FileReader.java
+++ HW Assign 1/src/FileReader.java
@@ -4,17 +4,40 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
- * <p><\p>
+ * <p>
+ * This is the File Reading and Character matching and word substitution
+ * assignment<\p>
+ * 
  * @author rileyp
  *
  */
 public class FileReader {
+
+	private static String outputFile = "output.txt";
+
+	public static void main(String[] args) {
+		
+		/*if (args.length < 3) {
+			System.out.println("Not enough files my dude");
+			System.exit(1);
+		}*/
+		
+		PrintWriter out = fileWrite(outputFile);
+
+		Scanner in1 = fileRead(args[0], 1);
+		if (checkBraces(in1))
+			out.println("Braces Balanced\n");
+		else
+			out.println("Braces Not Balanced\n");
+	}
+
 	/**
 	 * 
 	 * @return Scanner a scanner of the txt file
-	 * @param fname is a file name
+	 * @param fname
+	 *            is a file name
 	 */
-	public static Scanner fileRead(String fname) {
+	public static Scanner fileRead(String fname, int index) {
 
 		File file = new File(fname);
 		Scanner input = null;
@@ -25,15 +48,30 @@ public class FileReader {
 
 		} catch (FileNotFoundException ex) {
 
-			System.out.println("Part 1: unable to Open File");
+			PrintWriter write = fileWrite(outputFile);
+			write.println("Part " + index + ": Unable to Open File");
 			return null;
 
 		}
-		
-		
 
 		return input;
 
+	}
+
+	public static boolean checkBraces(Scanner in) {
+		int openBrace = 0;
+		int closeBrace = 0;
+
+		while (in.hasNextLine()) {
+			char[] s = in.nextLine().toCharArray();
+			for (char c : s) {
+				if (c == '{')
+					openBrace++;
+				else if (c == '}')
+					closeBrace++;
+			}
+		}
+		return openBrace == closeBrace;
 	}
 
 	/**
@@ -60,30 +98,18 @@ public class FileReader {
 		return output;
 
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args) {
-		
-	}
 }
+
+/*
+ * public static void main(String[] args) { if (args.length < 2) {
+ * System.out.println("Not enough files my dude"); System.exit(1); }
+ * 
+ * Scanner file1 = fileRead(args[0]); if (file1 == null) System.exit(1);
+ * 
+ * Scanner file2 = fileRead(args[1]); if (file2 == null) System.exit(1);
+ * 
+ * //PrintWriter out = fileWrite(args[1]);
+ * 
+ * 
+ * in.close(); out.close(); }
+ */
